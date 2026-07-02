70f0099c4afc881ffe47cb6d8927c7df6d0ba415
Syatram <Syatram@outlook.com>
2026-07-02 14:47:10 -0500

    got rid of the unfinished AnimationHandler stuff

--- StarterPlayer/StarterPlayerScripts/Main.lua
+++ StarterPlayer/StarterPlayerScripts/Main.lua
@@ -1,8 +1,8 @@
 -- Quick implementation for testing. Don't expect it to stay like this. I'd prefer a 
 
+--!strict
 local Types = require("./Types")
 local StateStateMachine = require("./StateHandler/StateStateMachine")
-local AnimationHandler = require("./StateHandler/AnimationHandler")
 
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -11,22 +11,13 @@ local Main = {}
 Main.__index = Main
 
 local stateStateMachine = nil
-local FPSRig = nil
-
-local animationHandler = nil
 
 local function onCharacterAdded( character : Model )
 	FPSRig = ReplicatedStorage.FPSRig:Clone()
 	FPSRig.Parent = workspace
 	
-	character:WaitForChild("Humanoid")
-	print("Humanoid loaded")
-	
 	stateStateMachine = StateStateMachine.new(character)
 	stateStateMachine:init()
-	
-	animationHandler = AnimationHandler.new(stateStateMachine)
-	animationHandler:init(FPSRig)
 end
 
 local function onCharacterRemoving( character : Model )
